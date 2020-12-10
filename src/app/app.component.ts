5d58505fd749e0cd93357b51203dd1aa6cac08e6
luisrivas <luis.rivas@complexless.com-at-780911114768>
2020-12-10 16:54:25 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ export class AppComponent implements OnInit {
             let choosenDev;
             for (const dev of videoDevices){
               alert(dev+ " dev.label.  "+ dev.label) 
-                if (dev.label.includes('front')){
+                if (dev.label.includes('back')){
                     choosenDev = dev;
                     break;
                 }
@@ -43,7 +43,7 @@ export class AppComponent implements OnInit {
                   JSON.stringify(videoDevices[1]))
             if (choosenDev) {
               alert(1+ "---"+JSON.stringify(choosenDev) )
-                this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+                this.qrScannerComponent.chooseCamera.next(videoDevices[1]);
             } else {
               alert(2 + "---"+ JSON.stringify(videoDevices[0]) +"--"+ JSON.stringify(videoDevices[2]))
                 this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
